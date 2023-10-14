fe6bad8a84ba9058644320e36533dddb7dfc13f4
robert-juru <juru0188@gmail.com>
2023-10-14 22:46:11 +0300

    Add current weather details functionality

--- src/index.js
+++ src/index.js
@@ -4,19 +4,29 @@ const apiKey = 'cd21f6b65bf347d6a4e142840231310';
 let location = document.querySelector('.location').textContent;
 let apiUrl = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=3`;
 let searchBar = document.querySelector('input[type="search"]');
+//unity of measure for data
+let tempUnit = "&deg;C";
+let tempUnitSup = "<sup>&degC</sup>";
+let windUnit = " km/h";
+let probabilityUnit = "%";
 
 async function searchLocation(event) {
     if (event.key === 'Enter') {
         try {
-            const currentWeather = await fetchCurrentWeather();
-            document.querySelector('.location').textContent = currentWeather.location;
-            document.querySelector('.date').textContent = currentWeather.formattedLocalTime;
-            document.querySelector('.today-degrees').innerHTML = currentWeather.temperatureCelsius +'<sup>&degC</sup>';
-            document.querySelector('.current-hour').textContent = currentWeather.localHour;
-            document.querySelector('.current-temp-interval').textContent = currentWeather.tempInterval;
-            document.querySelector('.current-weather-condition').textContent = currentWeather.weatherCondition;
-            document.querySelector('.sunrise-hour').textContent = currentWeather.sunrise;
-            document.querySelector('.sunset-hour').textContent = currentWeather.sunset;
+            const weatherData = await fetchWeatherData();
+            document.querySelector('.location').textContent = weatherData.location;
+            document.querySelector('.date').textContent = weatherData.formattedLocalTime;
+            document.querySelector('.today-degrees').innerHTML = weatherData.temperatureCelsius + tempUnitSup;
+            document.querySelector('.current-hour').textContent = weatherData.localHour;
+            document.querySelector('.current-temp-interval').textContent = weatherData.tempInterval;
+            document.querySelector('.current-weather-condition').textContent = weatherData.weatherCondition;
+            document.querySelector('.sunrise-hour').textContent = weatherData.sunrise;
+            document.querySelector('.sunset-hour').textContent = weatherData.sunset;
+            document.getElementById('perceived-temperature-value').innerHTML = weatherData.perceivedTemperature + tempUnit;
+            document.getElementById('rain-probability-value').textContent = weatherData.currentRainProbability + probabilityUnit;
+            document.getElementById('wind-speed-value').textContent = weatherData.windSpeed + windUnit;
+            document.getElementById('air-humidity-value').textContent = weatherData.airHumidity + probabilityUnit;
+            document.getElementById('uv-index-value').textContent = weatherData.uvIndex;
         } catch (error) {
             console.error('An error occurred:', error.message);
         }
@@ -24,23 +34,27 @@ async function searchLocation(event) {
 }
 
 async function searchInitialLocation() {
-        try {
-            const currentWeather = await fetchCurrentWeather();
-            console.log(currentWeather)
-            document.querySelector('.location').textContent = currentWeather.location;
-            document.querySelector('.date').textContent = currentWeather.formattedLocalTime;
-            document.querySelector('.today-degrees').innerHTML = currentWeather.temperatureCelsius +'<sup>&degC</sup>';
-            document.querySelector('.current-hour').textContent = currentWeather.localHour;
-            document.querySelector('.current-temp-interval').textContent = currentWeather.tempInterval;
-            document.querySelector('.current-weather-condition').textContent = currentWeather.weatherCondition;
-            document.querySelector('.sunrise-hour').textContent = currentWeather.sunrise;
-            document.querySelector('.sunset-hour').textContent = currentWeather.sunset;
-        } catch (error) {
-            console.error('An error occurred:', error.message);
-        }
+    try {
+        const weatherData = await fetchWeatherData();
+        document.querySelector('.location').textContent = weatherData.location;
+        document.querySelector('.date').textContent = weatherData.formattedLocalTime;
+        document.querySelector('.today-degrees').innerHTML = weatherData.temperatureCelsius + '<sup>&degC</sup>';
+        document.querySelector('.current-hour').textContent = weatherData.localHour;
+        document.querySelector('.current-temp-interval').textContent = weatherData.tempInterval;
+        document.querySelector('.current-weather-condition').textContent = weatherData.weatherCondition;
+        document.querySelector('.sunrise-hour').textContent = weatherData.sunrise;
+        document.querySelector('.sunset-hour').textContent = weatherData.sunset;
+        document.getElementById('perceived-temperature-value').innerHTML = weatherData.perceivedTemperature + tempUnit;
+        document.getElementById('rain-probability-value').textContent = weatherData.currentRainProbability + probabilityUnit;
+        document.getElementById('wind-speed-value').textContent = weatherData.windSpeed + windUnit;
+        document.getElementById('air-humidity-value').textContent = weatherData.airHumidity + probabilityUnit;
+        document.getElementById('uv-index-value').textContent = weatherData.uvIndex;
+    } catch (error) {
+        console.error('An error occurred:', error.message);
     }
+}
 
-async function fetchCurrentWeather() {
+async function fetchWeatherData() {
     if (searchBar.value !== '') {
         location = searchBar.value;
         apiUrl = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=3`;
@@ -48,7 +62,9 @@ async function fetchCurrentWeather() {
     try {
         const response = await fetch(apiUrl);
         const data = await response.json();
+        console.log(data)
 
+        // main current weather + location
         const cityName = data.location.name;
         const countryName = data.location.country;
         location = cityName + ', ' + countryName;
@@ -62,9 +78,25 @@ async function fetchCurrentWeather() {
         const formattedLocalTime = format(parseISO(localTime), 'EEEE, MMMM dd yyyy')
         const sunrise = data.forecast.forecastday[0].astro.sunrise;
         const sunset = data.forecast.forecastday[0].astro.sunset;
+        // const dailyChanceOfRain = data.forecast.forecastday[0].day.daily_chance_of_rain; - using it for forecast for 3 days
 
-        let currentWeather = { location, formattedLocalTime, localHour, tempInterval, weatherCondition, temperatureCelsius, sunrise, sunset }
-        return currentWeather;
+        //real-time weather details
+        const perceivedTemperature = Math.round(data.current.feelslike_c);
+        let currentRainProbability = null;
+        const hourlyForecasts = data.forecast.forecastday[0].hour;
+        for (let i = 0; i < hourlyForecasts.length; i++) {
+            let hourForecast = hourlyForecasts[i].time.split(' ')[1];
+            if (localHour.substring(0, 2) === hourForecast.substring(0, 2)) {
+                currentRainProbability = hourlyForecasts[i].chance_of_rain;
+            }
+        }
+        const windSpeed = Math.round(data.current.wind_kph);
+        const airHumidity = data.current.humidity;
+        const uvIndex = data.current.uv;
+
+        let weatherData = { location, formattedLocalTime, localHour, tempInterval, weatherCondition, temperatureCelsius, sunrise, sunset, perceivedTemperature, currentRainProbability, windSpeed, airHumidity, uvIndex }
+
+        return weatherData;
     }
     catch (error) {
         console.error('An error occurred:', error.message);
